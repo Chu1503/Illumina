64de14c4638a1e013500ee4dbb14849bf68f8bc7
Shiv Akash <shivakash2003@gmail.com>
2023-05-21 02:51:39 +0530

    added tilting elements for speakers

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import "./Speakers.css"
+import React, { useEffect } from 'react';
+import './Speakers.css';
 
 const Speakers = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `${process.env.PUBLIC_URL}/vanilla-tilt.js`;
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <header>
-    <div className='speakers'>
+      <div className='speakers'>
         <h1 className='title-speakers'>SPEAKERS</h1>
-    </div>
-  </header>
-  )
-}
+        <div className='cards'>
+          <div className='tiltcard' data-tilt data-tilt-glare data-tilt-max="10" data-tilt-startX="20" data-tilt-startY="-20" data-tilt-reset-to-start="true" data-tilt-scale="1.1"></div>
+          <div className='tiltcard' data-tilt data-tilt-glare data-tilt-max="10"></div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Speakers
+export default Speakers;
