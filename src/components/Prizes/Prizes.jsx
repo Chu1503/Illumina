25bc391f48b42707cca520828826a2217ce50297
Shiv Akash <shivakash2003@gmail.com>
2023-05-21 03:56:53 +0530

    prizes page

--- src/components/Prizes/Prizes.jsx
+++ src/components/Prizes/Prizes.jsx
@@ -6,6 +6,11 @@ const Prizes = () => {
     <header>
     <div className='prizes'>
         <h1 className='title-prizes'>PRIZES</h1>
+    <div className="medals">
+      <div className="second"></div>
+      <div className="first"></div>
+      <div className="third"></div>
+    </div>
     </div>
   </header>
   )
--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import "./Sponsors.css"
+import React, { useEffect } from 'react';
+import './Sponsors.css';
 
 const Sponsors = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `${process.env.PUBLIC_URL}/vanilla-tilt.js`;
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <header>
-    <div className='sponsors'>
+      <div className='sponsors'>
         <h1 className='title-sponsors'>SPONSORS</h1>
-    </div>
-  </header>
-  )
-}
+        <div className="sponsor-cards">
+          <div className="sponsor-tilt" data-tilt data-tilt-glare></div>
+          <div className="sponsor-tilt" data-tilt data-tilt-glare></div>
+          <div className="sponsor-tilt" data-tilt data-tilt-glare></div>
+          <div className="sponsor-tilt" data-tilt data-tilt-glare></div>
+          <div className="sponsor-tilt" data-tilt data-tilt-glare></div>
+          <div className="sponsor-tilt" data-tilt data-tilt-glare></div>
+          <div className="sponsor-tilt" data-tilt data-tilt-glare></div>
+          <div className="sponsor-tilt" data-tilt data-tilt-glare data-tilt-max="10"></div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Sponsors
+export default Sponsors;
