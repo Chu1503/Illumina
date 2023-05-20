352383ba1a2743d33c1798aec5f3b80000795715
Shiv Akash <shivakash2003@gmail.com>
2023-05-21 02:15:03 +0530

    automatic image carousel in about iei page

--- src/components/AboutIEI/AboutIEI.jsx
+++ src/components/AboutIEI/AboutIEI.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import "./AboutIEI.css"
+import Carousel from './Carousel'
 
 const AboutIEI = () => {
   return (
     <header>
+          <link href='https://use.fontawesome.com/releases/v5.11.2/css/all.css'/>
     <div className='aboutIEI'>
       <h1 className='title-about-iei'>ABOUT IE(I)</h1>
-      <div className='cta'>
-      
+      <div className="container">
+        <Carousel>
+          <img src='./images/1_vedikaglitch.png' alt='' style={{width: "100%", height: "100%"}}></img>
+          <img src='./images/6_arishaglitch.png' alt='' style={{width: "100%", height: "100%"}}></img>
+          <img src='./images/5_chuglitch.png' alt='' style={{width: "100%", height: "100%"}}></img>
+          <img src='./images/7_sanskritiglitch.png' alt='' style={{width: "100%", height: "100%"}}></img>
+          <img src='./images/10_shreyaglitch.png' alt='' style={{width: "100%", height: "100%"}}></img>
+        </Carousel>
       </div>
-      <div className='content-about-illumina'>
+      <div className='content-about-iei'>
         <p>
-        Lorem ipsum dolor sit amet. Eum architecto dolorum vel quaerat laboriosam ut nemo maiores et quam voluptatem 
-        et commodi placeat. Aut quia dolorem quo iste accusantium et quas accusamus ab animi dicta ab magni consequatur. 
-        Et alias molestias qui velit voluptatem et velit atque est voluptas quaerat non sint vitae? Qui ratione dolore 
-        est quaerat officiis a obcaecati sequi eos voluptate rerum est tempora obcaecati id commodi ipsa vel ducimus quia. 
-        Sed fugiat deserunt est excepturi unde aut suscipit sunt ut dolorum debitis ut blanditiis dolor est officiis sapiente. 
-        Eum voluptas dicta aut consequatur nemo ut nisi ducimus 33 illum eligendi sed error consequatur. Vel galisum maiores
-        et distinctio adipisci quo accusantium necessitatibus. Qui enim rerum ab Quis corrupti non mollitia error qui saepe 
-        vitae eos itaque velit. Ut rerum vero officia architecto in necessitatibus magnam nulla rerum quo delectus architecto.
-        Aut ducimus officiis aut labore illum est ratione voluptas mister chu.
+        The Institution of Engineers (India) is a non-profit national organisation that was founded in 1920. 
+        Our objective is to effectively enhance engineering, engineering sciences, and technology, as well as their applications, 
+        in India. We are dedicated to increasing technological knowledge, skill, and ability among our members and the broader public.
+        As the world's largest professional association of engineers, IE(I) implements cutting-edge tactics to advance causes such 
+        as sustainable development.In VIT, the IE(I) student chapter acts as the forum for technical and non-technical development 
+        and achievements.
+        The broad reach of IE(I) VIT enables it to reach a large and diverse audience and to hold a variety of technical events 
+        and activities on a regular basis, including skill-building workshops, ideation events, and hackathons.
         </p>
       </div>
       </div>
@@ -28,3 +35,22 @@ const AboutIEI = () => {
 }
 
 export default AboutIEI
+
+
+// const slideContainer = document.querySelector('.container');
+// const slide = document.querySelector('.slider');
+// const prevBtn = document.getElementById('prev-btn');
+// const nextBtn = document.getElementById('next-btn');
+
+// let slides = document.querySelectorAll('.slide');
+// let index = 1;
+
+// const firstClone = slides[0].cloneNode(true);
+// const lastClone = slides[slides.length - 1].cloneNode(true);
+
+// firstClone.id = 'first-clone';
+// lastClone.id = 'last-clone';
+
+// slide.append(firstClone);
+// slide.prepend(lastClone);
+
--- src/components/AboutIllumina/AboutIllumina.jsx
+++ src/components/AboutIllumina/AboutIllumina.jsx
@@ -7,7 +7,7 @@ const AboutIllumina = () => {
       <div className='aboutIllumina'>
         <h1 className='title-about-illumina'>ABOUT ILLUMINA</h1>
       <div className='cta'>
-      <div className='Logo'><img src="https://cdn.discordapp.com/attachments/894801439992475768/1109183336334762096/logo.png" width="300" alt="LOGO" style={{ display: 'block', margin: '0 auto' }}/></div>
+      <div className='Logo'><img src="./images/logo.png" width="300" alt="LOGO" style={{ display: 'block', margin: '0 auto' }}/></div>
       </div>
       <div className='content-about-illumina'>
         <p>
