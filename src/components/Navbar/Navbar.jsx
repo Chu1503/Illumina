dce45bc1fecd3d52c359771526ff0e0be7c383af
Harsh Bishnoi <bishnoi4@illinois.edu>
2023-05-26 00:13:37 -0500

    navigation bar without clicks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,17 @@ import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div classname = "NavBar">
+      <ul>
+        <li><a href="#home">HOME</a></li>
+        <li><a href="www.google.com">ABOUT US</a></li>
+        <li><a href="#timeline">TIMELINE</a></li>
+        <li><a href="#prizes">PRIZES</a></li>
+        <li><a href="#sponsors">SPONSORS</a></li>
+        <li><a href="#contactUs">CONTACT US</a></li>
+      </ul>
+    </div>
+
   )
 }
 
