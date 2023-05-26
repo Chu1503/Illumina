e907752b1d9dcd71f031dc5f6b91c04d431b175b
Harsh Bishnoi <bishnoi4@illinois.edu>
2023-05-26 00:36:55 -0500

    navigation bar non clickable

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,6 @@ import Timeline from "./Timeline/Timeline";
 import Prizes from "./Prizes/Prizes";
 import Sponsors from "./Sponsors/Sponsors";
 import ContactUs from "./ContactUs/ContactUs";
+import Navbar from "./Navbar/Navbar";
 
-export { LandingPage, AboutIllumina, AboutIEI, Speakers, Timeline, Prizes, Sponsors, ContactUs };
+export { LandingPage, AboutIllumina, AboutIEI, Speakers, Timeline, Prizes, Sponsors, ContactUs, Navbar };
