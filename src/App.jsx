bc10e435fb9d30bd3e718d93113306778ed61c93
Shiv Akash <shivakash2003@gmail.com>
2023-05-31 23:18:46 +0530

    parallax update

--- src/App.jsx
+++ src/App.jsx
@@ -22,62 +22,63 @@ function App() {
       <Parallax pages={9} style={{ top: '0', left: '0' }} class="animation">
 
         <Navbar></Navbar>
-        <ParallaxLayer offset={0} speed={0.35}>
+
+        <ParallaxLayer offset={0} speed={0.01}>
           <div class="animation_layer parallax" id="auroraB"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.45}>
+        <ParallaxLayer offset={0} speed={0.2}>
           <div class="animation_layer parallax" id="front6"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.25}>
+        <ParallaxLayer offset={0} speed={0.48}>
           <div class="animation_layer parallax" id="front5"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.15}>
+        <ParallaxLayer offset={0} speed={0.40}>
           <div class="animation_layer parallax" id="front4"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.55}>
+        <ParallaxLayer offset={0} speed={0.16}>
           <div class="animation_layer parallax" id="front3"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.65}>
+        <ParallaxLayer offset={0} speed={0.4}>
           <div class="animation_layer parallax" id="front2"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.5}>
+        <ParallaxLayer offset={0} speed={0.31}>
           <div class="animation_layer parallax" id="front1"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.05}>
+        <ParallaxLayer offset={0} speed={0.36}>
           <div class="animation_layer parallax" id="treesF"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={2} speed={0}>
+        <ParallaxLayer offset={1} speed={0}>
           <AboutIllumina />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={3} speed={0}>
+        <ParallaxLayer offset={2} speed={0}>
           <AboutIEI />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={4} speed={0}>
+        <ParallaxLayer offset={3} speed={0}>
           <Speakers />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={5} speed={0}>
+        <ParallaxLayer offset={4} speed={0}>
           <Timeline />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={6} speed={0}>
+        <ParallaxLayer offset={5} speed={0}>
           <Prizes />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={7} speed={0}>
+        <ParallaxLayer offset={6} speed={0}>
           <Sponsors />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={8} speed={0}>
+        <ParallaxLayer offset={7} speed={0}>
           <ContactUs />
         </ParallaxLayer>
 
--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -18,7 +18,8 @@ const Speakers = () => {
       <div className='speakers'>
         <h1 className='title-speakers'>SPEAKERS</h1>
         <div className='cards'>
-          <div className='tiltcard' data-tilt data-tilt-glare data-tilt-max="10" data-tilt-startX="20" data-tilt-startY="-20" data-tilt-reset-to-start="true" data-tilt-scale="1.1"></div>
+          <div className='tiltcard' data-tilt data-tilt-glare data-tilt-max="10"></div>
+          <div className='tiltcard' data-tilt data-tilt-glare data-tilt-max="10"></div>
           <div className='tiltcard' data-tilt data-tilt-glare data-tilt-max="10"></div>
         </div>
       </div>
